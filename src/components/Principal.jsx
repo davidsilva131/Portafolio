86f974c70fcc109fd3a3ac695697e8b6ea557d43
David Silva <davidsilvac131@gmail.com>
2023-05-09 12:28:06 -0500

    Update principal styles and content

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,7 +1,10 @@
 const Principal = () => {
   return (
-    <section id='home' className='mt-24 h-screen'>
-      <h1>Hola</h1>
+    <section id='home' className='mt-24 h-full w-full flex justify-center py-4 px-4 lg:px-52'>
+    <aside className='flex flex-col gap-3 items-center'>
+      <h1 className='font-bold text-4xl'>David Silva</h1>
+      <h2 className='font-base text-sm opacity-70'>FrontEnd Developer</h2>
+    </aside>
     </section>
   )
 }
