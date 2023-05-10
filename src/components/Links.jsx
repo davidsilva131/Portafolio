f7b907a841d637957e82b15280286391393612ee
David Silva <davidsilvac131@gmail.com>
2023-05-09 19:09:22 -0500

    Update styles

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,5 +1,5 @@
 'use client'
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 
 const Links = ({ open, setOpen }) => {
   const pages = [
@@ -12,29 +12,25 @@ const Links = ({ open, setOpen }) => {
       href: 'projects',
     },
     {
-      text:'contact',
-      href:'contact'
+      text: 'contact',
+      href: 'contact'
     }
   ]
   return (
     <div
-      className={`transition-all ease-out duration-500 lg:transition-none ${
-        open ? 'h-screen' : 'h-0'
-      }  items-center w-full lg:flex  lg:h-14 lg:w-auto lg:order-1`}
-      id='navbar-sticky'
+      className={`transition-all ease-out duration-500 lg:transition-none ${open ? 'h-screen' : 'h-0'
+        }  items-center w-full lg:flex  lg:h-14 lg:w-auto lg:order-1`}
     >
       <ul
-        className={`${
-          !open && 'hidden'
-        } lg:flex duration-300 ease-out lg:transition-none flex flex-col p-4 lg:p-0 mt-4 font-medium bg-transparent lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0 lg:bg-transparent`}
+        className={`${!open && 'hidden'
+          } lg:flex duration-300 ease-out lg:transition-none flex flex-col p-4 lg:p-0 mt-4 font-medium bg-transparent lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0 lg:bg-transparent`}
       >
         {pages.map(page => (
           <li key={page.text}>
             <Link
               to={page.href}
-              spy={true}
               smooth={true}
-              className='block cursor-pointer py-2 pl-3 pr-4 text-white rounded lg:bg-transparent lg:text-white lg:p-0 uppercase hover:text-green-500 hover:transition-all hover:duration-700'
+              className='block cursor-pointer py-2 pl-3 pr-4 text-white hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-green-400 hover:to-purple-500 rounded lg:bg-transparent lg:text-white lg:p-0 uppercase hover:text-green-500 hover:transition-all hover:duration-700'
             >
               {page.text}
             </Link>
--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -2,16 +2,16 @@ import Scene from "./Scene/Scene"
 
 const Principal = () => {
   return (
-    <section id='home' className='mt-24 h-full w-full flex flex-col lg:flex-row lg:justify-center lg:gap-24 xl:gap-48 gap-5 items-center py-4 px-4 lg:px-52'>
-    <aside className='flex flex-col gap-3 items-center lg:justify-start'>
-      <h1 className='font-bold text-4xl flex flex-col items-center lg:items-start lg:text-4xl text-[#22C55E] xl:text-6xl'>
-      David Silva
-      <span className='font-base text-white text-sm lg:text-xl opacity-70'>FrontEnd Developer</span>
-      </h1>
-    </aside>
-    <aside className='items-center w-96 h-96'>
-      <Scene/>
-    </aside>
+    <section id='home' className='mt-56 h-full w-full flex flex-col lg:flex-row lg:justify-center lg:gap-24 xl:gap-48 gap-5 items-center py-4 px-4 lg:px-52'>
+      <aside className='flex flex-col gap-3 items-center lg:justify-start'>
+        <h1 className='font-bold text-4xl flex flex-col items-center lg:items-start lg:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-purple-500 xl:text-6xl'>
+          David Silva
+          <span className='font-base text-white text-sm lg:text-xl opacity-70'>FrontEnd Developer</span>
+        </h1>
+      </aside>
+      <aside className='items-center w-80 h-96'>
+        <Scene />
+      </aside>
     </section>
   )
 }
