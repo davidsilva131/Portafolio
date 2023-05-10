29d21ec7e52b724bdf7e6b106e8d19602700018e
David Silva <davidsilvac131@gmail.com>
2023-05-10 16:48:03 -0500

    Add footer component with styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className='mt-56 w-full py-12 flex justify-center'>
+      <h4 className='text-sm font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-purple-500'><span>© </span>Made by David Silva</h4>
+    </footer>
+  )
+}
+
+export default Footer
