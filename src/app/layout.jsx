1e68d1937b3eec242058623e4e9b6221885894e5
David Silva <davidsilvac131@gmail.com>
2023-05-10 17:01:09 -0500

    Delete head import

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,6 @@ import NavBar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
-import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
