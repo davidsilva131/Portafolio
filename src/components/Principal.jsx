5e3dcb5be335658fcfc3ba1e589805c681681442
David Silva <davidsilvac131@gmail.com>
2023-05-10 16:51:23 -0500

    Update pricipal component

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -6,7 +6,8 @@ const Principal = () => {
       <aside className='flex flex-col gap-3 items-center lg:justify-start'>
         <h1 className='font-bold text-4xl flex flex-col items-center lg:items-start lg:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-purple-500 xl:text-6xl'>
           David Silva
-          <span className='font-base text-white text-sm lg:text-xl opacity-70'>FrontEnd Developer</span>
+          <span className='font-base text-white text-xs lg:text-lg opacity-70'>FrontEnd Developer</span>
+          <span className='mt-4 font-base text-center lg:text-left text-white text-sm lg:text-xl'>Make and get better web to be accessible to everyone</span>
         </h1>
       </aside>
       <aside className='items-center w-80 h-96'>
