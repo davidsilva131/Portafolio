eb42a7648f683e920cc3c6ec51671ecbd3adaa06
David Silva <davidsilvac131@gmail.com>
2023-05-09 19:08:47 -0500

    Update styles

--- src/components/HamburguerButton.jsx
+++ src/components/HamburguerButton.jsx
@@ -9,19 +9,16 @@ const HamburgerButton = ({ open, setOpen }) => {
     >
       <div className='block w-5 absolute left-6 top-1/2   transform  -translate-x-1/2 -translate-y-1/2'>
         <span
-          className={`${
-            open ? 'rotate-45' : '-translate-y-1.5'
-          } block absolute h-0.5 w-7 text-green-500 bg-current transform transition duration-500 ease-in-out`}
+          className={`${open ? 'rotate-45' : '-translate-y-1.5'
+            } block absolute h-0.5 w-7 text-green-500 bg-current transform transition duration-500 ease-in-out`}
         />
         <span
-          className={`${
-            open && 'opacity-0'
-          }  absolute  h-0.5 w-5 block text-green-500 bg-current transform transition duration-500 ease-in-out`}
+          className={`${open && 'opacity-0'
+            }  absolute  h-0.5 w-5 block text-green-500 bg-current transform transition duration-500 ease-in-out`}
         />
         <span
-          className={`${
-            open ? '-rotate-45' : 'translate-y-1.5'
-          } block absolute  h-0.5 w-7 text-green-500 bg-current transform  transition duration-500 ease-in-out`}
+          className={`${open ? '-rotate-45' : 'translate-y-1.5'
+            } block absolute  h-0.5 w-7 text-green-500 bg-current transform  transition duration-500 ease-in-out`}
         />
       </div>
     </button>
