4e1f8eb19f1d5e0184b28e32d9568393b94f7c8f
David Silva <davidsilvac131@gmail.com>
2023-05-10 16:50:38 -0500

    Fix scroll and update tab index

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -25,12 +25,15 @@ const Links = ({ open, setOpen }) => {
         className={`${!open && 'hidden'
           } lg:flex duration-300 ease-out lg:transition-none flex flex-col p-4 lg:p-0 mt-4 font-medium bg-transparent lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0 lg:bg-transparent`}
       >
-        {pages.map(page => (
+        {pages.map((page, index) => (
           <li key={page.text}>
             <Link
+              tabIndex={index + 1}
               to={page.href}
               smooth={true}
+              offset={-120}
               className='block cursor-pointer py-2 pl-3 pr-4 text-white hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-green-400 hover:to-purple-500 rounded lg:bg-transparent lg:text-white lg:p-0 uppercase hover:text-green-500 hover:transition-all hover:duration-700'
+              onClick={() => { setOpen(false) }}
             >
               {page.text}
             </Link>
