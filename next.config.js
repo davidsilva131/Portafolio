64872e39e8cf4de25a7220b0e8e936873c8831df
David Silva <davidsilvac131@gmail.com>
2023-05-10 16:53:59 -0500

    Update next configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: ['icongr.am'],
+    formats: ['image/avif', 'image/webp']
+  }
+}
 
 module.exports = nextConfig
