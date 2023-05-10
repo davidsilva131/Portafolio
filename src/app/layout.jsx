d9b91a72de2b2a1d042ce61225223345c0849779
David Silva <davidsilvac131@gmail.com>
2023-05-10 16:54:37 -0500

    Add footer component to layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,20 +1,23 @@
 import NavBar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'David Silva',
-  description: 'Juan David Silva fron end developer portfolio.',
+  description: 'Juan David Silva fron end developer portfolio.'
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout ({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-black text-white`}>
-        <NavBar/>
+        <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   )
