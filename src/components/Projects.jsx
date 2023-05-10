d75c2c9963831524288d28396563860fa4a9f499
David Silva <davidsilvac131@gmail.com>
2023-05-09 19:10:02 -0500

    Update styles and components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,14 @@
+import ProjectsCards from "./ProjectsCards"
+
 const Projects = () => {
   return (
-    <section id='projects' className='mt-24 h-60'>
-      <h1>Projects</h1>
+    <section id='projects' className='mt-56 w-full flex flex-col items-center py-4 px-4 lg:px-52'>
+      <div>
+        <h2 className='text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-purple-500'>
+          Projects
+        </h2>
+      </div>
+      <ProjectsCards />
     </section>
   )
 }
