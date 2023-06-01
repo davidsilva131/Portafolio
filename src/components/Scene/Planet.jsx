d781d9d65fb82022b8ab754991140058986e85ad
David Silva <davidsilvac131@gmail.com>
2023-06-01 10:50:27 -0500

    Add typescript to my skills

--- src/components/Scene/Planet.jsx
+++ src/components/Scene/Planet.jsx
@@ -1,25 +1,49 @@
 'use client'
-import { useMemo } from "react"
+import { useMemo } from 'react'
 import * as THREE from 'three'
-import Word from "./Word"
+import Word from './Word'
 
 export default function Planet({ count = 5, radius = 20 }) {
-  const skills=['React','Sass','Redux','Tailwindcss','Next.js','Threejs','JavaScript','SQL','Git','Firebase','Firestore','Supabase','Axios','C#','HTML','CSS','Material UI']
+  const skills = [
+    'React',
+    'Sass',
+    'Redux',
+    'Tailwindcss',
+    'Next.js',
+    'Threejs',
+    'JavaScript',
+    'Typescript',
+    'SQL',
+    'Git',
+    'Firebase',
+    'Firestore',
+    'Supabase',
+    'Axios',
+    'C#',
+    'HTML',
+    'CSS',
+    'Material UI'
+  ]
 
-  const randomSkill=()=>{
-    return skills[Math.floor(Math.random()*skills.length)]
+  const randomSkill = () => {
+    return skills[Math.floor(Math.random() * skills.length)]
   }
   const words = useMemo(() => {
     const temp = []
     const spherical = new THREE.Spherical()
     const phiSpan = Math.PI / (count + 1)
-    const thetaSpan = (Math.PI*2) / count
-    for (let i = 1; i < count+1; i++)
-      for (let j = 0; j < count; j++) temp.push([new THREE.Vector3().setFromSpherical(spherical.set(radius, phiSpan * i, thetaSpan * j)), randomSkill()])
+    const thetaSpan = (Math.PI * 2) / count
+    for (let i = 1; i < count + 1; i++)
+      for (let j = 0; j < count; j++)
+        temp.push([
+          new THREE.Vector3().setFromSpherical(
+            spherical.set(radius, phiSpan * i, thetaSpan * j)
+          ),
+          randomSkill()
+        ])
     return temp
   }, [count, radius])
-  return (
-      words.map(([pos, word], index) => <Word key={index} position={pos} children={word}/>)
-  )
-  
+  return words.map(([pos, word], index) => (
+    <Word key={index} position={pos} children={word} />
+  ))
 }
